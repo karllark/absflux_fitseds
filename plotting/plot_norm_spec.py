6a2ac21ab7d91a88e2f15414ccef6653b8cbeaf6
Karl Gordon <kgordon@stsci.edu>
2025-08-21 16:38:27 -0400

    code for ploting data/model normalized by the continuum model

---
+++ plotting/plot_norm_spec.py
@@ -0,0 +1,187 @@
+import argparse
+import pickle
+import emcee
+import matplotlib.pyplot as plt
+import numpy as np
+
+from measure_extinction.model import MEModel
+from measure_extinction.stardata import StarData
+from measure_extinction.extdata import ExtData
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("starname", help="Name of star")
+    parser.add_argument("--burnfrac", help="burn fraction", default=0.5, type=float)
+    parser.add_argument(
+        "--obspath",
+        help="path to observed data",
+        default="/home/kgordon/Python/extstar_data/MW/",
+    )
+    parser.add_argument(
+        "--picmodname", help="name of pickled model", default="tlusty_z100_modinfo.p"
+    )
+    parser.add_argument(
+        "--bands", help="only use these observed bands", nargs="+", default=None
+    )
+    parser.add_argument("--png", help="save figure as a png file", action="store_true")
+    parser.add_argument("--pdf", help="save figure as a pdf file", action="store_true")
+    args = parser.parse_args()
+
+    # base filename
+    outname = f"figs/{args.starname}_mefit"
+
+    # get the observed data
+    fstarname = f"{args.starname}.dat"
+    reddened_star = StarData(fstarname, path=f"{args.obspath}", only_bands=args.bands)
+
+    # get the modeling info
+    modinfo = pickle.load(open(args.picmodname, "rb"))
+    modinfo_cont = pickle.load(open(args.picmodname.replace("modinfo", "cont_modinfo"), "rb"))
+
+    # setup the ME model
+    memod = MEModel(obsdata=reddened_star, modinfo=modinfo)
+    memod_cont = MEModel(obsdata=reddened_star, modinfo=modinfo_cont)
+
+    # get the extinction curve
+    tstr = outname.replace("figs", "exts")
+    extfile = f"{tstr}_ext.fits"
+    ext = ExtData(filename=extfile)
+    ptab = ext.fit_params["MCMC"]
+    for k, cname in enumerate(ptab["name"]):
+        param = getattr(memod, cname)
+        param.value = ptab["value"][k]
+        if ptab["fixed"][k] > 0.1:
+            param.fixed = True
+        else:
+            param.fixed = False
+        if ptab["prior"][k] > 0.1:
+            param.prior = (ptab["prior_val"][k], ptab["prior_unc"][k])
+        else:
+            param.prior = None
+
+    # weights
+    memod.fit_weights(reddened_star)
+    memod_cont.fit_weights(reddened_star)
+
+    # get the MCMC chains
+    sampfile = f"{tstr}_.h5"
+    reader = emcee.backends.HDFBackend(sampfile)
+    samples = reader.get_chain()
+    nsteps = samples.shape[0]
+
+    # analyze chains for convergence
+    tau = reader.get_autocorr_time(quiet=True)
+    print("taus = ", tau)
+    # burnin = int(2 * np.max(tau))
+    # thin = int(0.5 * np.min(tau))
+    # samples = reader.get_chain(discard=burnin, flat=True, thin=thin)
+    # log_prob_samples = reader.get_log_prob(discard=burnin, flat=True, thin=thin)
+    # log_prior_samples = reader.get_blobs(discard=burnin, flat=True, thin=thin)
+
+    # print("burn-in: {0}".format(burnin))
+    # print("thin: {0}".format(thin))
+    # print("flat chain shape: {0}".format(samples.shape))
+    # print("flat log prob shape: {0}".format(log_prob_samples.shape))
+    # print("flat log prior shape: {0}".format(log_prior_samples.shape))
+
+    flat_samples = reader.get_chain(discard=int(args.burnfrac * nsteps), flat=True)
+
+    # get the 50 percentile and +/- uncertainties
+    params_per = map(
+        lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
+        zip(*np.percentile(flat_samples, [16, 50, 84], axis=0)),
+    )
+
+    # now package the fit parameters into two vectors, averaging the +/- uncs
+    n_params = samples.shape[2]
+    params_p50 = np.zeros(n_params)
+    params_unc = np.zeros(n_params)
+    for k, val in enumerate(params_per):
+        params_p50[k] = val[0]
+        params_unc[k] = 0.5 * (val[1] + val[2])
+
+    # set the best fit parameters in the output model
+    memod.fit_to_parameters(params_p50, uncs=params_unc)
+    memod_cont.fit_to_parameters(params_p50, uncs=params_unc)
+    print(f"p50 parameters, burnfrac={args.burnfrac}")
+    memod.pprint_parameters()
+
+    # plot
+    fig, axes = plt.subplots(nrows=3, figsize=(8, 8))
+
+    # normalized the data by the continuum model
+    modsed = memod.stellar_sed(modinfo)
+    ext_modsed = memod.dust_extinguished_sed(modinfo, modsed)
+    hi_ext_modsed = memod.hi_abs_sed(modinfo, ext_modsed)
+
+    modsed_cont = memod_cont.stellar_sed(modinfo_cont)
+    ext_modsed_cont = memod_cont.dust_extinguished_sed(modinfo_cont, modsed_cont)
+    hi_ext_modsed_cont = memod_cont.hi_abs_sed(modinfo_cont, ext_modsed_cont)
+
+    for cspec in reddened_star.data.keys():
+
+        if cspec == "BAND":
+            ptype = "o"
+            rcolor = "k"
+        else:
+            ptype = "-"
+            rcolor = "k"
+
+        if cspec == "BAND":
+            cwaves = reddened_star.data[cspec].waves
+        else:
+            cwaves = modinfo.waves[cspec]
+
+        reddened_star.data[cspec].fluxes /= (memod_cont.norm.value * hi_ext_modsed_cont[cspec])
+        reddened_star.data[cspec].uncs /= (memod_cont.norm.value * hi_ext_modsed_cont[cspec])
+        hi_ext_modsed[cspec] /= hi_ext_modsed_cont[cspec]
+
+        # nan models where no data or excluded
+        nvals = np.full(len(cwaves), 1.0)
+        nvals[memod_cont.weights[cspec] == 0.0] = np.nan
+        gvals = reddened_star.data[cspec].fluxes > 0.0
+
+        for cax in axes:
+            cax.plot(
+                cwaves, hi_ext_modsed[cspec] * nvals, "r" + ptype
+            )
+            cax.plot(
+                cwaves, hi_ext_modsed[cspec], "r" + ptype, alpha=0.3,
+            )
+
+            # data
+            cax.errorbar(
+                reddened_star.data[cspec].waves[gvals],
+                reddened_star.data[cspec].fluxes[gvals],
+                yerr=reddened_star.data[cspec].uncs[gvals],
+                fmt="b" + ptype,
+                alpha=0.5,
+            )
+
+    axes[0].set_xlim(0.1, 0.3)
+    axes[1].set_xlim(0.3, 1.0)
+    axes[2].set_xlim(1.0, 2.0)
+
+    for cax in axes:
+        cax.axhline(1.0, ls="--", color="k", alpha=0.5)
+        cax.set_ylabel(r"$F/F_{\mathrm{modcont}}$")
+        cax.set_ylim(0.5, 1.2)
+
+    axes[2].set_xlabel(r"$\lambda$ [$\mu$m]")
+
+    save_str = "_mefit_mcmc_norm"
+
+    fig.tight_layout()
+
+    # plot or save to a file
+    if args.png:
+        fig.savefig(f"{args.starname}{save_str}.png")
+    elif args.pdf:
+        fig.savefig(f"{args.starname}{save_str}.pdf")
+    else:
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/pic_cont.py
@@ -0,0 +1,33 @@
+import glob
+import pickle
+import time
+
+from measure_extinction.modeldata import ModelData
+
+
+if __name__ == "__main__":
+    # model data
+    start_time = time.time()
+    print("reading model files")
+    modstr = "wd_hubeny_"
+    modpath = "/home/kgordon/Python/extstar_data/Models/"
+
+    tlusty_models_fullpath = glob.glob(f"{modpath}/{modstr}*cont.dat")
+    tlusty_models = [
+        tfile[tfile.rfind("/") + 1 : len(tfile)] for tfile in tlusty_models_fullpath
+    ]
+    if len(tlusty_models) > 1:
+        print(f"{len(tlusty_models)} model files found.")
+    else:
+        raise ValueError("no model files found.")
+
+    # get the models with just the reddened star band data and spectra
+    modinfo = ModelData(
+        tlusty_models,
+        path=f"{modpath}/",
+        band_names=None,
+        spectra_names=["BAND", "STIS", "STIS_Opt"],
+    )
+    pickle.dump(modinfo, open(f"{modstr}_cont_modinfo.p", "wb"))
+    print("finished reading model files")
+    print("--- %s seconds ---" % (time.time() - start_time))
