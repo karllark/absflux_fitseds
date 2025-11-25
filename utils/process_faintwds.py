0b3d7c2daa88dac4d6f08bacf5b4f169cde2919d
Karl Gordon <kgordon@stsci.edu>
2025-11-25 10:18:41 -0500

    adding optical gratings

--- utils/process_faintwds.py
+++ utils/process_faintwds.py
@@ -10,10 +10,16 @@ from measure_extinction.merge_obsspec import (
 
 if __name__ == "__main__":
 
-    names = ["wdfs0122_30", "wdfs1302_10"]
+    # fmt: off
+    names = ["wdfs0122_30", "wdfs0248_33", "wdfs0458_56", "wdfs0639_57", "wdfs0956_38", "wdfs1055_36",
+             "wdfs1110_17", "wdfs1206_27", "wdfs1214_45", "wdfs1302_10", "wdfs1434_28", "wdfs1514_00",
+             "wdfs1535_77", "wdfs1557_55", "wdfs1814_78", "wdfs1837_70", "wdfs1930_52", "wdfs2317_29",
+             "wdfs2351_37"]
+    # fmt: on
 
     path = "data/faintwds/"
     for cname in names:
+        print(f"working on {cname}")
 
         for cspec in ["wfc3_g102", "wfc3_g141"]:
             grating = cspec.split("_")[1]
@@ -26,6 +32,17 @@ if __name__ == "__main__":
                 rb_info = merge_gen_obsspec(
                     [tab], obsspecinfo[cspec][1], output_resolution=cres
                 )
+
+                ofile = f"{path}{cname}_{cspec}.fits"
+                rb_info.write(ofile, overwrite=True)
+
+        for cspec in ["stis_g430l", "stis_g750l"]:
+            grating = cspec.split("_")[1]
+            cfile = f"{path}stis/{cname}.{grating}"
+            if os.path.exists(cfile):
+                tab = read_bohlin(cfile)
+
+                rb_info = merge_stis_obsspec([tab], waveregion="Opt")
 
                 ofile = f"{path}{cname}_{cspec}.fits"
                 rb_info.write(ofile, overwrite=True)
