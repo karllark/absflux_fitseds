e761885a1231598b68e4879fc8ecab77ec8c07f1
Karl Gordon <kgordon@stsci.edu>
2025-11-25 16:24:54 -0500

    adding miscwds

--- utils/process_hstdata.py
+++ utils/process_hstdata.py
@@ -12,6 +12,7 @@ from measure_extinction.merge_obsspec import (
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--medwds", help="process WD stars (mediumwds)", action="store_true")
+    parser.add_argument("--miscwds", help="process misc WD stars (miscwds)", action="store_true")
     args = parser.parse_args()
 
 
@@ -22,6 +23,10 @@ if __name__ == "__main__":
                  "wd2149_021",
                  "hz4", "wd1202_232", "wd1544_377", "wd2341_322"]   # hz4 = WD0352_096
         path = "data/mediumwds/"
+    elif args.miscwds:
+        names = ["grw_70d5824", "sdss132811", "wd1057_719", "wd1657_343",
+                 "wdj040027", "wdj041345", "wdj174911", "wdj175318", "wdj181144"]
+        path = "data/miscwds/"
     else:
         names = ["wdfs0122_30", "wdfs0248_33", "wdfs0458_56", "wdfs0639_57", "wdfs0956_38", "wdfs1055_36",
                 "wdfs1110_17", "wdfs1206_27", "wdfs1214_45", "wdfs1302_10", "wdfs1434_28", "wdfs1514_00",
@@ -61,9 +66,11 @@ if __name__ == "__main__":
 
         for cspec in ["stis_g140l", "stis_g230l"]:
             grating = cspec.split("_")[1]
-            tab = read_bohlin(f"{path}stis/{cname}.{grating}")
+            cfile = f"{path}stis/{cname}.{grating}"
+            if os.path.exists(cfile):
+                tab = read_bohlin(cfile)
 
-            rb_info = merge_stis_obsspec([tab], waveregion="UV")
+                rb_info = merge_stis_obsspec([tab], waveregion="UV")
 
-            ofile = f"{path}{cname}_{cspec}.fits"
-            rb_info.write(ofile, overwrite=True)
+                ofile = f"{path}{cname}_{cspec}.fits"
+                rb_info.write(ofile, overwrite=True)
