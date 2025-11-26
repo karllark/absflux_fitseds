ce35af5e5ad53214f606ae29665820a4aa93dd95
Karl Gordon <kgordon@stsci.edu>
2025-11-26 10:46:08 -0500

    less output

--- utils/check_hstdata.py
+++ utils/check_hstdata.py
@@ -18,9 +18,7 @@ if __name__ == "__main__":
             file1 = f"{path}{cfile}"
             file2 = f"{rpath}{cfile}"
             if os.path.exists(file2):
-                if filecmp.cmp(file1, file2, shallow=False):
-                    print(f"{cfile} is up-to-date")
-                else:
+                if not filecmp.cmp(file1, file2, shallow=False):
                     shutil.copyfile(file2, file1)
                     print(f"{cfile} updated from origin dir")
             else:
